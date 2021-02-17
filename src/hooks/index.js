4d398a401e28a5e1c65c17ecae91aa7a5b481dbe
Mohib <mohammadm@springrainit.com>
2021-02-17 15:51:43 +0600

    refactor folder name

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useUsers } from "./useUsers";
+export { default as useUser } from "./useUser";
+export { default as useAxios } from "./useAxios";
+export { default as useAuth } from "./useAuth";
---
+++ src/hooks/useAuth.js
@@ -0,0 +1,51 @@
+import { createContext, useState, useContext, useMemo } from "react";
+import { useMutation } from "react-query";
+import axios from "axios";
+import { apiEndpoint } from "../config";
+
+export const AuthContext = createContext();
+
+export function AuthProvider(props) {
+  const [token, setToken] = useState();
+
+  const { mutate, isLoading } = useMutation(
+    data => axios.post(`${apiEndpoint}/login`, data),
+    {
+      onSuccess: data => {
+        setToken(data.data);
+      },
+    }
+  );
+
+  const login = data => {
+    mutate(data);
+    setToken("data.data");
+  };
+
+  const logout = () => {
+    setToken(null);
+  };
+
+  const contextValue = useMemo(
+    () => ({
+      isAuth: !!token,
+      token: token,
+      login: login,
+      logout: logout,
+      isLoading: isLoading,
+    }),
+
+    // eslint-disable-next-line
+    [token, isLoading]
+  );
+
+  return <AuthContext.Provider value={contextValue} {...props} />;
+}
+
+export default function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error(`useAuth must be used within a AuthProvider`);
+  }
+  return context;
+}
